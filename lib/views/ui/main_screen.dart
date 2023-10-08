0a04a16e29f23560ecf76778cbe3b6f8ae37306d
Kartono666 <kartono.saleh@gmail.com>
2023-10-08 19:40:22 +0700

    update main screen

--- lib/views/ui/main_screen.dart
+++ lib/views/ui/main_screen.dart
@@ -1,10 +1,11 @@
-import 'package:e_commerce_flutter/views/shared/app_style.dart';
-import 'package:e_commerce_flutter/views/shared/bottom_nav_widget.dart';
+import 'package:e_commerce_flutter/controllers/main_screen_provider.dart';
+import 'package:e_commerce_flutter/views/shared/bottom_nav_bar.dart';
 import 'package:e_commerce_flutter/views/ui/cart_page.dart';
 import 'package:e_commerce_flutter/views/ui/home_page.dart';
 import 'package:e_commerce_flutter/views/ui/profile_page.dart';
 import 'package:e_commerce_flutter/views/ui/search_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
@@ -19,49 +20,13 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int pageIndex = 2;
-
-    /// 18:58
-    return Scaffold(
-      backgroundColor: Color(0xFFE2E2E2),
-      body: pageList[pageIndex],
-      bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Container(
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(16))),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                BottomNavWidget(
-                  onTap: () {},
-                  icon: Icons.home,
-                ),
-                BottomNavWidget(
-                  onTap: () {},
-                  icon: Icons.search,
-                ),
-                BottomNavWidget(
-                  onTap: () {},
-                  icon: Icons.add,
-                ),
-                BottomNavWidget(
-                  onTap: () {},
-                  icon: Icons.shopping_cart,
-                ),
-                BottomNavWidget(
-                  onTap: () {},
-                  icon: Icons.person,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return Consumer<MainScreenNotifier>(
+      builder: (context, mainScreenNotifier, child) {
+        return Scaffold(
+          body: pageList[mainScreenNotifier.pageIndex],
+          bottomNavigationBar: const BottomNavBar(),
+        );
+      },
     );
   }
 }
