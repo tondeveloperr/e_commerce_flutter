3bd3a4f608b0db2a350d4f262b31e4f58463bea7
Kartono666 <kartono.saleh@gmail.com>
2023-10-08 19:39:20 +0700

    update home page

--- lib/views/ui/home_page.dart
+++ lib/views/ui/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce_flutter/views/shared/app_style.dart';
+import 'package:e_commerce_flutter/views/shared/product_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,14 +10,169 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final TabController _tabController =
+      TabController(length: 3, vsync: this);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Home Page",
-          style: appstyle(40, Colors.black, FontWeight.bold),
+      backgroundColor: Color(0xFFE2E2E2),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage("assets/images/top_image.png"),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(left: 8, bottom: 15),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Athletics Shoes",
+                        style: appstyleWithHt(
+                            42, Colors.white, FontWeight.bold, 1.5)),
+                    Text("Collection",
+                        style: appstyleWithHt(
+                            42, Colors.white, FontWeight.bold, 1.2)),
+                    TabBar(
+                        indicatorSize: TabBarIndicatorSize.label,
+                        indicatorColor: Colors.transparent,
+                        controller: _tabController,
+                        isScrollable: true,
+                        labelColor: Colors.white,
+                        labelStyle: appstyle(24, Colors.white, FontWeight.bold),
+                        unselectedLabelColor: Colors.grey.withOpacity(0.3),
+                        tabs: const [
+                          Tab(
+                            text: "Men Shoes",
+                          ),
+                          Tab(
+                            text: "Women Shoes",
+                          ),
+                          Tab(
+                            text: "Kids Shoes",
+                          ),
+                        ]),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.265),
+              child: Container(
+                padding: const EdgeInsets.only(left: 12),
+                child: TabBarView(controller: _tabController, children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return ProductCard(
+                                id: "1",
+                                name: "Sepatu",
+                                price: "200.000",
+                                image:
+                                    "https://d326fntlu7tb1e.cloudfront.net/uploads/710d020f-2da8-4e9e-8cff-0c8f24581488-GV6674.webp",
+                                category: "Men Shoes",
+                              );
+                            }),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 20, 12, 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Latest Shoes",
+                                  style: appstyle(
+                                      24, Colors.black, FontWeight.bold),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "Show All",
+                                      style: appstyle(
+                                          22, Colors.black, FontWeight.w500),
+                                    ),
+                                    const Icon(
+                                      Icons.navigate_next,
+                                      size: 24,
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.13,
+                        child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(16)),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.white,
+                                            spreadRadius: 1,
+                                            blurRadius: 0.8,
+                                            offset: Offset(0, 1))
+                                      ]),
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.12,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.28,
+                                  child: CachedNetworkImage(
+                                    imageUrl:
+                                        "https://d326fntlu7tb1e.cloudfront.net/uploads/58282ea3-b815-4d26-9f4f-382aa62f67cf-HP5404_a1.webp",
+                                  ),
+                                ),
+                              );
+                            }),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.405,
+                        color: Colors.green,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.4,
+                        color: Colors.pink,
+                      )
+                    ],
+                  ),
+                ]),
+              ),
+            )
+          ],
         ),
       ),
     );
