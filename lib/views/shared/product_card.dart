dbc537a87d20f6017c480bd6c10d6e3b621cd538
Kartono666 <kartono.saleh@gmail.com>
2023-10-08 19:36:43 +0700

    Membuat product card widget

---
+++ lib/views/shared/product_card.dart
@@ -0,0 +1,115 @@
+import 'package:e_commerce_flutter/views/shared/app_style.dart';
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatefulWidget {
+  const ProductCard(
+      {super.key,
+      required this.id,
+      required this.name,
+      required this.price,
+      required this.image,
+      required this.category});
+
+  final String id;
+  final String name;
+  final String price;
+  final String image;
+  final String category;
+
+  @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  @override
+  Widget build(BuildContext context) {
+    bool selected = true;
+    return Padding(
+      padding: EdgeInsets.fromLTRB(8, 0, 20, 0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width * 0.6,
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                color: Colors.white,
+                spreadRadius: 1,
+                blurRadius: 0.6,
+                offset: Offset(1, 1))
+          ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.23,
+                    decoration: BoxDecoration(
+                        image:
+                            DecorationImage(image: NetworkImage(widget.image))),
+                  ),
+                  Positioned(
+                    right: 10,
+                    top: 10,
+                    child: GestureDetector(
+                      onTap: null,
+                      child: Icon(Icons.heart_broken_rounded),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.name,
+                      style: appstyleWithHt(
+                          36, Colors.black, FontWeight.bold, 1.1),
+                    ),
+                    Text(
+                      widget.category,
+                      style:
+                          appstyleWithHt(18, Colors.grey, FontWeight.bold, 1.5),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 8, right: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.price,
+                      style: appstyle(30, Colors.black, FontWeight.w600),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Colors",
+                          style: appstyle(18, Colors.grey, FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        ChoiceChip(
+                          label: Text(" "),
+                          selected: selected,
+                          visualDensity: VisualDensity.compact,
+                          selectedColor: Colors.black,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
